67702abbf0f3101482ec345ab856d559ca708cca
Demo User <demouser@MacBook-Air.local>
2017-07-25 10:18:26 -0700

    added new file with time_calc function

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import jinja2
 import os
 from google.appengine.api import users
 import logging
+from time_calc_funct import time_calc
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
@@ -70,31 +71,8 @@ class FinancialLogHandler(webapp2.RequestHandler):
         userID = user.user_id()
 
         #Total Time Worked:
-        def time_calc(clock_in_hour,clock_out_hour,clock_in_min,clock_out_min,time_of_day_in,time_of_day_out):
-            total_min = 0
-            total_hour = 0
-            total_time = 0
-            if time_of_day_in.upper().trim()=='AM' and time_of_day_out.upper().trim()=='AM' or time_of_day_in.upper().trim()=='PM' and time_of_day_out.upper().trim()=='PM':
-                if clock_in_hour == clock_out_hour:
-                    total_time = clock_out_min - clock_in_minute
-                    total_time = (total_time / 60.0)
-                else:
-                    total_min = (60 - clock_in_minute) + clock_out_min
-                    # This is included as part of the mathematics >
-                    clock_in_hour += 1
-                    total_hour = clock_out_hour - clock_in_hour
-                    total_time = total_hour + (total_min / 60.0)
-            elif time_of_day_in.upper().trim()=='AM' and time_of_day_out.upper().trim()=='PM':
-                clock_out_hour += 12
-                total_min = (60 - clock_in_min) + clock_out_min
-                clock_in_hour +=1
-                total_hour = clock_out_hour - clock_in_hour
-                total_time = total_hour + (total_min / 60.0)
-            else:
-                logging.info('calculator failure')
-            return total_time
-
         time_worked = time_calc(clock_in_hour,clock_out_hour,clock_in_min,clock_out_min,time_of_day_in,time_of_day_out)
+        
         total tax = 0
         if marital_status == 2:
             total_tax = 6.20 + 1.45 + 0.90
