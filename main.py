9925f134b3f3e7aa36708d7e45a7c984e7411ae2
Demo User <demouser@MacBook-Air.local>
2017-07-24 22:47:20 -0700

    added time calculator function

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import webapp2
 import jinja2
 import os
 from google.appengine.api import users
+import logging
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
@@ -57,19 +58,50 @@ class FinancialLogHandler(webapp2.RequestHandler):
 
         #Request the Wage Variables
         clock_in_hour = int(self.request.get('time_in_hour'))
-        clock_in_minute = int(self.request.get('time_in_min'))
+        clock_in_min = int(self.request.get('time_in_min'))
         time_of_day_in = (self.request.get('time_of_day_in'))
 
         clock_out_hour = int(self.request.get('time_out_hour'))
         clock_out_min = int(self.request.get('time_out_min'))
         time_of_day_out = (self.request.get('time_of_day_out'))
+        
         break_time_length = int(self.request.get('break_time_length'))
         marital_status = int(self.request.get('marital_status'))
         userID = user.user_id()
 
         #Total Time Worked:
-        if time_of_day_in.upper().trim()=='AM':
-            
+    def time_calc(clock_in_hour,clock_out_hour,clock_in_min,clock_out_min,time_of_day_in,time_of_day_out):
+        total_min = 0
+        total_hour = 0
+        total_time = 0
+        if time_of_day_in.upper().trim()=='AM' and time_of_day_out.upper().trim()=='AM' or time_of_day_in.upper().trim()=='PM' and time_of_day_out.upper().trim()=='PM':
+            if clock_in_hour == clock_out_hour:
+                total_time = clock_out_min - clock_in_minute
+                total_time = (total_time / 60.0)
+            else:
+                total_min = (60 - clock_in_minute) + clock_out_min
+                # This is included as part of the mathematics >
+                clock_in_hour += 1
+                total_hour = clock_out_hour - clock_in_hour
+                total_time = total_hour + (total_min / 60.0)
+        elif time_of_day_in.upper().trim()=='AM' and time_of_day_out.upper().trim()=='PM':
+            clock_out_hour += 12
+            total_min = (60 - clock_in_min) + clock_out_min
+            clock_in_hour +=1
+            total_hour = clock_out_hour - clock_in_hour
+            total_time = total_hour + (total_min / 60.0)
+        else:
+            logging.info('calculator failure')
+        return total_time
+
+
+
+
+
+
+
+
+
 
 
 
